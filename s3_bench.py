81c023b577bf9a0080af35331ab562375c0ee958
Your Name <you@example.com>
2023-03-27 11:31:55 +0300

    initial commit

---
+++ s3_bench.py
@@ -0,0 +1,30 @@
+import boto3
+import time
+import os
+
+# Set up S3 client
+session = boto3.Session(profile_name='ab')
+s3 = session.client('s3')
+
+# Define S3 bucket and file to download
+bucket_name = 'ml-bucket01'
+file_key = 'md/hk/BNN/GALABUSD.md/1669513140970565.data'
+
+# Define local file path to save the downloaded file
+local_file_path = '1670782505139937.data'
+
+# Download the file and measure the download speed
+start_time = time.time()
+
+s3.download_file(bucket_name, file_key, local_file_path, Config=boto3.s3.transfer.TransferConfig(max_concurrency=20))
+
+end_time = time.time()
+download_time = end_time - start_time
+
+# Calculate the download speed
+file_size = os.path.getsize(local_file_path)
+download_speed = file_size / download_time / 1024 / 1024 # Convert to MB/s
+
+# Print the download speed
+print(f"Downloaded {file_size} bytes in {download_time} seconds")
+print(f"Download speed: {download_speed} MB/s")
